ad4bb4972589b0fb481d0617ee22de64e3ff83e9
Kristen Norris <krnorris65@gmail.com>
2018-01-27 11:43:43 -0600

    created function to create horizontal and vertical walls/paths by passing in how many blocks, and what position you want the walls/paths at

--- world-manager.js
+++ world-manager.js
@@ -7,55 +7,60 @@ const WorldManager = (function () {
       walls.enableBody = true;
       let wall;
 
-      //size of block
-      let block = 32;
- 
+      //vertical border walls
       for(let w = 0; w < 24; w++){
         //left border
-        wall = walls.create(0, block * w, 'wall');
+        wall = walls.create(0, TILE_HEIGHT * w, 'wall');
         //right border
-        wall = walls.create(1280-block, block * w, 'wall');
+        wall = walls.create(1280-TILE_WIDTH, TILE_HEIGHT * w, 'wall');
         wall.body.immovable = true;
       }
 
+      //horizontal border walls
       for(let w = 0; w < 40; w++){
         //top border
-        wall = walls.create(block * w, 0, 'wall');
+        wall = walls.create(TILE_WIDTH * w, 0, 'wall');
         //bottom border
-        wall = walls.create(block * w, 720-block, 'wall');
+        wall = walls.create(TILE_WIDTH * w, 720-TILE_HEIGHT, 'wall');
         wall.body.immovable = true;
       }
-      
-      //creates a horizontal wall
+
+      //function to create a horizontal wall
       function hWall(num, x, y){
         for(let w = 0; w < num; w++){
-          wall = walls.create(x + (block * w), y, 'wall');
+          wall = walls.create(x + (TILE_WIDTH * w), y, 'wall');
           wall.body.immovable = true;
         }
-      }
+      };
 
-      //creates a vertical wall
+      //function to create a vertical wall
       function vWall(num, x, y){
         for(let w = 0; w < num; w++){
-          wall = walls.create(x, y + (block * w), 'wall');
+          wall = walls.create(x, y + (TILE_HEIGHT * w), 'wall');
           wall.body.immovable = true;
         }
-      }
-
-
-
-
-
-
+      };
+      
 
       let paths = game.add.group();
       paths.enableBody = true;
+      
+      //function to create a horizontal path
+      function hPath(num, x, y){
+        for(let p = 0; p < num; p++){
+          path = paths.create(x + (TILE_WIDTH * p), y, 'path');
+          path.body.immovable = true;
+        }
+      };
 
-      let path = paths.create(32, 64, 'path');
-      path.body.immovable = true;
+      //function to create a vertical path
+      function vPath(num, x, y){
+        for(let p = 0; p < num; p++){
+          path = paths.create(x, y + (TILE_HEIGHT * p), 'path');
+          path.body.immovable = true;
+        }
+      };
 
-      path = paths.create(64, 32, 'path');
-      path.body.immovable = true;
 
 
     },
