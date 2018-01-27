162ee20310f3f89bf6d6b545a455d2a8e5fb67ce
Kristen Norris <krnorris65@gmail.com>
2018-01-27 11:13:50 -0600

    working on creating a function to build walls

--- world-manager.js
+++ world-manager.js
@@ -1,16 +1,38 @@
 const WorldManager = (function () {
   return {
     levelHeight: 24,
-    levelWidth: 32,
+    levelWidth: 40,
     level1Update() {
       let walls = game.add.group();
       walls.enableBody = true;
+      let wall;
 
-      let wall = walls.create(64, 64, 'wall');
-      wall.body.immovable = true;
+      //size of block
+      let block = 32;
+ 
+      for(let w = 0; w < 24; w++){
+        //left border
+        wall = walls.create(0, block * w, 'wall');
+        //right border
+        wall = walls.create(1280-block, block * w, 'wall');
+        wall.body.immovable = true;
+      }
 
-      wall = walls.create(32, 32, 'wall');
-      wall.body.immovable = true;
+      for(let w = 0; w < 40; w++){
+        //top border
+        wall = walls.create(block * w, 0, 'wall');
+        //bottom border
+        wall = walls.create(block * w, 720-block, 'wall');
+        wall.body.immovable = true;
+      }
+
+      //walls.create(x, y, asset)
+     wall = walls.create(block*2, block*4, 'wall');
+
+
+
+
+
 
       let paths = game.add.group();
       paths.enableBody = true;
@@ -20,6 +42,8 @@ const WorldManager = (function () {
 
       path = paths.create(64, 32, 'path');
       path.body.immovable = true;
+
+
     },
   };
 })();
