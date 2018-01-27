6a306db044d9d242b7a94c44fe834d6e343591ea
Kristen Norris <krnorris65@gmail.com>
2018-01-27 16:12:17 -0600

    building level 1 walls

--- world-manager.js
+++ world-manager.js
@@ -1,15 +1,16 @@
 let walls;
+let wall;
+
+let paths;
+let path;
+
 
 const WorldManager = (function () {
   return {
     levelHeight: 22,
     levelWidth: 40,
-    level1Update() {
-      let wall;
 
-      walls = game.add.group();
-      walls.enableBody = true;
-
+    buildBorder(){
       //vertical border walls
       for(let w = 0; w < this.levelHeight; w++){
         //left border
@@ -29,45 +30,59 @@ const WorldManager = (function () {
         wall = walls.create(TILE_WIDTH * w, game.world.height-TILE_HEIGHT, 'wall');
         wall.body.immovable = true;
       }
+    },
+    //function to create a horizontal wall
+    hWall(num, x, y){
+      for(let w = 0; w < num; w++){
+        wall = walls.create(x + (TILE_WIDTH * w), y, 'wall');
+        wall.body.immovable = true;
+      }
+    },
 
-      //function to create a horizontal wall
-      function hWall(num, x, y){
-        for(let w = 0; w < num; w++){
-          wall = walls.create(x + (TILE_WIDTH * w), y, 'wall');
-          wall.body.immovable = true;
-        }
-      };
+    //function to create a vertical wall
+    vWall(num, x, y){
+      for(let w = 0; w < num; w++){
+        wall = walls.create(x, y + (TILE_HEIGHT * w), 'wall');
+        wall.body.immovable = true;
+      }
+    },
 
-      //function to create a vertical wall
-      function vWall(num, x, y){
-        for(let w = 0; w < num; w++){
-          wall = walls.create(x, y + (TILE_HEIGHT * w), 'wall');
-          wall.body.immovable = true;
-        }
-      };
+    //function to create a horizontal path
+    hPath(num, x, y){
+      for(let p = 0; p < num; p++){
+        path = paths.create(x + (TILE_WIDTH * p), y, 'path');
+        path.body.immovable = true;
+      }
+    },
 
+    //function to create a vertical path
+    vPath(num, x, y){
+      for(let p = 0; p < num; p++){
+        path = paths.create(x, y + (TILE_HEIGHT * p), 'path');
+        path.body.immovable = true;
+      }
+    },
 
-      let paths = game.add.group();
+    level1Update() {
+
+      walls = game.add.group();
+      walls.enableBody = true;
+      
+      paths = game.add.group();
       paths.enableBody = true;
+      
+      this.buildBorder();
+      this.hWall(8, 32*6, 32*16);
+      
+      this.vWall(9, 32*20, 32*3);
+      this.hWall(12, 32*20, 32*3);
+      
+      this.vWall(8, 32*20, 32*15);
+      this.hWall(8, 32*21, 32*15);
+      
+      // for (let i = 1; i < this.levelHeight - 1; i++ )
+      // this.hPath(this.levelWidth - 2, 1 * TILE_WIDTH, i * TILE_HEIGHT);
 
-      //function to create a horizontal path
-      function hPath(num, x, y){
-        for(let p = 0; p < num; p++){
-          path = paths.create(x + (TILE_WIDTH * p), y, 'path');
-          path.body.immovable = true;
-        }
-      };
-
-      //function to create a vertical path
-      function vPath(num, x, y){
-        for(let p = 0; p < num; p++){
-          path = paths.create(x, y + (TILE_HEIGHT * p), 'path');
-          path.body.immovable = true;
-        }
-      };
-
-      for (let i = 1; i < this.levelHeight - 1; i++ )
-      hPath(this.levelWidth - 2, 1 * TILE_WIDTH, i * TILE_HEIGHT);
     },
   };
 })();
